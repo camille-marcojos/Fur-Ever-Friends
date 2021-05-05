2efb95babd6133949c2b9dfb16e9ac4884767274
cammychi <marcojoc@oregonstate.edu>
2021-05-05 13:55:03 -0700

    add credentials

---
+++ db_credentials.py
@@ -0,0 +1,8 @@
+# To actually have your app use this file, you need to RENAME the file to db_credentials.py
+# You will find details about your CS340 database credentials on Canvas.
+# the following will be used by the db_connector.py and 
+# in turn by all the Python code in this codebase to interact with the database
+host = 'localhost'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+user = 'root' #CHANGE ME
+passwd = 'Abbeygirl1' #CHANGE ME
+db = 'pet_match' #CHANGE ME
