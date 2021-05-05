bf18c2c4a3aa02d4a9200fe4d583abc90c09e68e
cammychi <marcojoc@oregonstate.edu>
2021-05-05 14:31:47 -0700

    db_credentials updated with database details

--- db_credentials.py
+++ db_credentials.py
@@ -1,8 +1,9 @@
 # To actually have your app use this file, you need to RENAME the file to db_credentials.py
 # You will find details about your CS340 database credentials on Canvas.
-# the following will be used by the db_connector.py and 
+# the following will be used by the db_connector.py and
 # in turn by all the Python code in this codebase to interact with the database
-host = 'localhost'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-user = 'root' #CHANGE ME
-passwd = 'Abbeygirl1' #CHANGE ME
-db = 'pet_match' #CHANGE ME
+# DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+host = 'us-cdbr-east-03.cleardb.com'
+user = 'ba5f914de3fbf7'  # CHANGE ME
+passwd = '94c866e4'  # CHANGE ME
+db = 'heroku_189095f41a2573d'  # CHANGE ME
