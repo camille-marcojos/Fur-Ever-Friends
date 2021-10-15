e98ecd24a2afdf6f1654342e5c9dd2e56f97755c
camille-marcojos <49379161+camille-marcojos@users.noreply.github.com>
2021-10-15 09:46:32 -0700

    Delete db_credentials.py

--- db_credentials.py
+++
@@ -1,9 +0,0 @@
-# To actually have your app use this file, you need to RENAME the file to db_credentials.py
-# You will find details about your CS340 database credentials on Canvas.
-# the following will be used by the db_connector.py and
-# in turn by all the Python code in this codebase to interact with the database
-# DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-host = 'us-cdbr-east-03.cleardb.com'
-user = 'ba5f914de3fbf7'  # CHANGE ME
-passwd = '94c866e4'  # CHANGE ME
-db = 'heroku_189095f41a2573d'  # CHANGE ME
